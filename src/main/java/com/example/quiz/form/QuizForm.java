30bd79641acc80d92e89b6506e81f518313c26cb
yosuke-portfolio <yosuke.kira.topia@gmail.com>
2023-09-07 02:01:57 +0900

    12章終了時点

---
+++ src/main/java/com/example/quiz/form/QuizForm.java
@@ -0,0 +1,24 @@
+package com.example.quiz.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuizForm {
+	/** 識別ID */
+	private Integer id;
+	/** クイズの内容 */
+	@NotBlank
+	private String question;
+	/** クイズの解答 */
+	private Boolean answer;
+	/** 作成者 */
+	@NotBlank
+	private String author;
+	/** 登録/変更 判定フラグ */
+	private Boolean newQuiz;
+}
